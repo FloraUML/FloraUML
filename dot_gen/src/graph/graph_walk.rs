7d2d22841af869db41768996604ba2c97b1be8da
Tim Seckinger <seckinger.tim@gmail.com>
2018-12-19 14:00:27 +0100

    dot_gen
    Co-authored-by: Jannis Kaiser <jannis.kaiser2@gmail.com>

---
+++ dot_gen/src/graph/graph_walk.rs
@@ -0,0 +1,39 @@
+use super::{Edge, Graph, Node};
+use std::borrow::Cow;
+
+impl<'a> dot::GraphWalk<'a, Node<'a>, Edge> for Graph<'a> {
+    fn nodes(&self) -> dot::Nodes<Node> {
+        Cow::Owned(self.0.values().cloned().collect())
+    }
+
+    fn edges(&self) -> dot::Edges<Edge> {
+        // fully connected undirected graph
+        Cow::Owned(
+            self.0
+                .keys()
+                .cloned()
+                .enumerate()
+                .flat_map(|(i, from)| {
+                    self.0
+                        .keys()
+                        .skip(i + 1)
+                        .cloned()
+                        .map(|to| (from, to))
+                        .collect::<Vec<_>>()
+                })
+                .collect(),
+        )
+    }
+
+    fn source(&self, edge: &Edge) -> Node {
+        self.0
+            .get(&edge.0)
+            .expect(&format!("Failed to find source node of edge {:?}", edge))
+    }
+
+    fn target(&self, edge: &Edge) -> Node {
+        self.0
+            .get(&edge.1)
+            .expect(&format!("Failed to find target node of edge {:?}", edge))
+    }
+}
--- dot_gen/src/lib.rs
+++ dot_gen/src/lib.rs
@@ -1,7 +1,47 @@
+mod graph;
+
+use crate::graph::model_to_graph;
+use dot::render;
+use flora_model::Model;
+use std::io;
+
+pub fn generate_dot<W: io::Write>(model: &Model, write: &mut W) -> io::Result<()> {
+    render(&model_to_graph(model), write)
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
+    fn get_dot(model: &Model) -> String {
+        let mut bytes = Vec::new();
+        assert!(generate_dot(&model, &mut bytes).is_ok());
+        String::from_utf8(bytes).expect("Generated DOT is invalid UTF-8")
+    }
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn empty() {
+        assert_eq!(
+            get_dot(&vec![]),
+            "graph g {
+}
+"
+        )
+    }
+
+    #[test]
+    fn fully_connected() {
+        assert_eq!(
+            get_dot(&vec!["A", "B", "C"]),
+            r#"graph g {
+    node_A[label="A"];
+    node_B[label="B"];
+    node_C[label="C"];
+    node_A -- node_B[label=""];
+    node_A -- node_C[label=""];
+    node_B -- node_C[label=""];
+}
+"#
+        )
     }
 }
