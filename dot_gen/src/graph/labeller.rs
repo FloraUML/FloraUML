e39ccdc9ae96a72fc4d0838e1115dc8691cc7116
Tim Seckinger <seckinger.tim@gmail.com>
2018-12-19 14:00:27 +0100

    dot_gen
    Co-authored-by: Jannis Kaiser <jannis.kaiser2@gmail.com>

---
+++ dot_gen/src/graph/labeller.rs
@@ -0,0 +1,21 @@
+use super::{Edge, Graph, Node};
+
+impl<'a> dot::Labeller<'a, Node<'a>, Edge> for Graph<'a> {
+    fn graph_id(&self) -> dot::Id {
+        dot::Id::new("g").unwrap()
+    }
+
+    // nodes
+
+    fn node_id(&self, node: &Node) -> dot::Id {
+        dot::Id::new(format!("node_{}", node)).expect(&format!("Invalid name for node: '{}'", node))
+    }
+
+    fn node_label(&self, node: &Node<'a>) -> dot::LabelText {
+        dot::LabelText::label(*node)
+    }
+
+    fn kind(&self) -> dot::Kind {
+        dot::Kind::Graph
+    }
+}
---
+++ dot_gen/src/graph/mod.rs
@@ -0,0 +1,14 @@
+mod graph_walk;
+mod labeller;
+
+use flora_model::Model;
+use std::collections::BTreeMap;
+
+pub struct Graph<'a>(BTreeMap<usize, Node<'a>>);
+
+type Node<'a> = &'a str;
+type Edge = (usize, usize);
+
+pub fn model_to_graph<'a>(model: &Model<'a>) -> Graph<'a> {
+    Graph(model.iter().cloned().enumerate().collect())
+}
